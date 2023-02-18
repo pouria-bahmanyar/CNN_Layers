f8e0123c890fbda01b8cd9ad883619e77dc71775
Pooria bahmanyar <pooriabahman@gmail.com>
2023-02-18 23:09:45 +0330

    updated

--- trainer.py
+++ trainer.py
@@ -1,58 +1,85 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from time import time
+import time
 import numpy as np
+import copy
+from tqdm.notebook import tqdm
+
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
 
-def train(model, epochs ,train_loader ,test_loader ,  opt, criterion, train_data, test_data):
-  losses = [] # Train Losses in different epochs
-  val_losses = []# Val Losses in different epochs
-  train_accuracy = []
-  val_accuracy = []
-  for epoch in range(epochs):
-    tic = time() # Start of epoch
-    # Train
-    model = model.train()
-    batch_losses = []
-    epoch_corrects = 0 # train corrects
-    for batch in train_loader:
-      input = batch[0].to(device) # Images
-      target = batch[1].to(device) # Labels
-      output = model.forward(input)
-      opt.zero_grad()
-      loss = criterion(output, target)
-      loss.backward()
-      opt.step()
-      batch_losses.append(loss.item())
-      batch_corrects = sum(target == output.argmax(dim = 1))
-      epoch_corrects += batch_corrects
-    epoch_loss = np.mean(batch_losses)
-    losses.append(epoch_loss)
-    epoch_accuracy = epoch_corrects / len(train_data)
-    train_accuracy.append(epoch_accuracy)
-    train_string = f"Epoch {epoch}, loss : {epoch_loss: 0.5f}, accuracy : {epoch_accuracy: 0.5f}, "
-    
-    model = model.eval() ### To disable dropout
-    # Validation
-    batch_losses = []
-    epoch_corrects = 0 # val corrects
-    for batch in test_loader:
-      input = batch[0].to(device) # Images
-      target = batch[1].to(device) # Labels
-      output = model.forward(input)
-      with torch.no_grad():
-        loss = criterion(output, target)
-        batch_losses.append(loss.item())
-        batch_corrects = sum(target == output.argmax(dim = 1))
-        epoch_corrects += batch_corrects
-    epoch_loss = np.mean(batch_losses)
-    val_losses.append(epoch_loss)
-    epoch_accuracy = epoch_corrects / len(test_data)
-    val_accuracy.append(epoch_accuracy)
-    toc = time() #End of epoch
-    val_string = f" val_loss : {epoch_loss: 0.5f}, val_accuracy : {epoch_accuracy: 0.5f}, time : {toc - tic: 0.1f}"
-    print(train_string + val_string)
+def train_model(model, dataloaders, criterion, optimizer, num_epochs):
+    since = time.time()
 
-  return model
+    test_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in (range(num_epochs)):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'test']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in tqdm(dataloaders[phase]):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    
+                    
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'test' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'test':
+                test_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best test Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, test_acc_history
     
